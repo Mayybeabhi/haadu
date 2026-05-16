dda1c71022ad2464473ccf2afa472e00f8549634
mayybeabhi <mayybeabhi@gmail.com>
2026-05-17 02:55:32 +0530

    Add environment-based configuration and CORS support

--- src/main/java/com/mayybeabhi/haadu/config/SecurityConfig.java
+++ src/main/java/com/mayybeabhi/haadu/config/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig  {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
