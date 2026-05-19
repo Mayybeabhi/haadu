cf866ee0fdf576f65b896b470ec1134e73360e5f
mayybeabhi <mayybeabhi@gmail.com>
2026-05-20 01:05:59 +0530

    Add vercel frontend CORS

--- src/main/java/com/mayybeabhi/haadu/config/CorsConfig.java
+++ src/main/java/com/mayybeabhi/haadu/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","http://localhost:5174")
+                        .allowedOrigins("http://localhost:5173","http://localhost:5174","https://haadu.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
